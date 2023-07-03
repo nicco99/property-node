61e80efc44a32a2a3138e37ad7c58574bcb49285
Nicholas Njeru <nicholasnjeru917@gmail.com>
2023-07-04 02:14:11 +0300

    updates

--- knexfile.js
+++ knexfile.js
@@ -15,21 +15,6 @@ module.exports = {
       directory: __dirname + "/knex/seeds",
     },
   },
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
 
   production: {
     client: "postgresql",
