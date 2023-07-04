5136c07666d2acc12e93ac8997c2e783ed211077
Nicholas Njeru <nicholasnjeru917@gmail.com>
2023-07-04 03:25:42 +0300

    aded cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const propertiesRouter = require("./properties/properties.router");
+var cors = require('cors')
 
+ 
+app.use(cors())
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/properties", propertiesRouter);
